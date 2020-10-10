0f69e24152e41bee7e7073864be1c664b2c17ef4
derk <derek.s@dealersunited.com>
2020-10-10 10:03:38 -0400

    Add links to breadcrumb component

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -2,9 +2,19 @@
   <div class="flex">
     <div v-for="(link, index) in links" :key="link">
       <div class="flex">
-        <div class="text-gray-500 text-sm leading-6 font-medium">
-          {{ link.text }}
-        </div>
+        <template v-if="link.name">
+          <router-link
+            :to="{ name: link.name }"
+            class="text-gray-500 text-sm leading-6 font-medium"
+          >
+            {{ link.text }}
+          </router-link>
+        </template>
+        <template v-else>
+          <div class="text-gray-500 text-sm leading-6 font-medium">
+            {{ link.text }}
+          </div>
+        </template>
         <div class="mx-4 text-gray-300" v-if="!(index == links.length - 1)">
           /
         </div>
